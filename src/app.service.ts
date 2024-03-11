45b3a35620abe70a713c86a0cd3dbba80d220a34
Valentín <61363314+Valo21@users.noreply.github.com>
2024-03-10 22:29:11 -0300

    test commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Docker image has been build and published by Github Actions!';
   }
 }
